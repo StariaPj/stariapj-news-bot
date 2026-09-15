f56fded39c0d92584aece4dc449f5f67c29d4d64
StariaPj <pj2gwk@gmail.com>
2026-09-15 19:18:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -368,8 +368,11 @@ def generate_html_email_body(data):
 """
     return html_code
 
-def send_email_with_pdf(pdf_bytes, report_data, recipients=["pj2gwk@gmail.com", "miyoungchoi88@gmail.com", "kimgiwoong5@gmail.com"]):
-    """지정된 수신자들(본인, 아내, 아들)에게 원본 링크가 적용된 HTML 이메일 및 PDF 동시 발송"""
+def send_email_with_pdf(pdf_bytes, report_data, recipients=None):
+    """지정된 수신자들에게 원본 링크가 적용된 HTML 이메일 및 PDF 동시 발송"""
+    if recipients is None:
+        recipients = ["pj2gwk@gmail.com", "miyoungchoi88@gmail.com", "kimgiwoong5@gmail.com"]
+        
     sender_user = os.environ.get("EMAIL_USER")
     sender_pass = os.environ.get("EMAIL_PASS")
     
@@ -879,7 +882,7 @@ if __name__ == "__main__":
     upload_to_gdrive(service, folder_id, pdf_bytes, report_data['time_str'])
     upload_json_to_gdrive(service, folder_id, report_data['new_cache'], report_data['time_str'])
     save_gdrive_cache(service, folder_id, report_data['new_cache'])
-    # 본인, 아내, 아들 세 분께 동시 발송
-    #send_email_with_pdf(pdf_bytes, report_data, recipients=["pj2gwk@gmail.com", "miyoungchoi88@gmail.com", "kimgiwoong5@gmail.com"])
-  # 본인 ONLY 발송 - Test mode
+    ## 본인, 아내, 아들 세 분께 동시 발송
+    # send_email_with_pdf(pdf_bytes, report_data, recipients=["pj2gwk@gmail.com", "miyoungchoi88@gmail.com", "kimgiwoong5@gmail.com"])
+    # 본인 ONLY
     send_email_with_pdf(pdf_bytes, report_data, recipients=["pj2gwk@gmail.com")
